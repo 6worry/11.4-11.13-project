b04f97da9a7fac15bcb6c9c4ad984da08c030aea
6worry <148040594+6worry@users.noreply.github.com>
2023-11-13 00:49:12 +0900

    Add files via upload

---
+++ project/order-item_data.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import {v4 as uuid} from 'uuid';
+import {readDataID, getDataID} from './sub_data.js';
+
+// //ID
+
+function orderitemID(){
+    return uuid();
+};
+
+// 주문ID
+
+let orderIDlist = readDataID('./order.csv');
+function equlorderID(orderid){
+    return getDataID(orderid);
+};
+
+//상품ID
+
+let itemIDlist = readDataID('./item.csv');
+function equlitemID(itemid){
+    return getDataID(itemid);
+};
+
+//주문 상품 데이터
+
+function orderitemResult(){
+    const result = `${orderitemID()}, ${equlorderID(orderIDlist)}, ${equlitemID(itemIDlist)}`;
+    return `${result}`;
+};
+
+//랜덤 데이터 생성
+
+let dataRecords = process.argv[2];
+let displayformat = process.argv[3];
+
+if(process.argv.length<4){
+    displayformat ='csv'; // 추후 csv 뿐만 아니라 다른 파일들도 생성 할 수도 있을거 같아 내버려둠
+};
+
+console.log('ID, 주문ID, 상품ID');
+
+const csvData = ['ID, 주문ID, 상품ID'];
+
+for (let i=0; i<dataRecords;i++){
+    const orderitemdata = orderitemResult();
+    console.log(orderitemdata);
+    csvData.push(orderitemdata); //csv에 데이터 심기
+};
+
+const csvString = csvData.join('\n');
+
+fs.writeFile('order-item.csv', csvString, 'utf-8', (err) => {
+    if (err) {
+    console.error('오류!오류!:', err);
+    } else {
+    console.log('CSV 생성 완료: order-item.csv');
+    };
+});
---
+++ project/order_data.js
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import {v4 as uuid} from 'uuid';
+import {readDataID, getDataID, generateDate} from './sub_data.js';
+
+//ID
+
+export function orderID(){
+    return uuid();
+};
+
+//주문일자
+
+function orderDate() {
+    const hour = (Math.floor(Math.random() * 24)).toString().padStart(2, 0);
+    const min = (Math.floor(Math.random() * 60)).toString().padStart(2, 0);
+    const sec = (Math.floor(Math.random() * 60)).toString().padStart(2, 0);
+
+    return `${'2023'}.${generateDate()} ${hour}:${min}:${sec}`;
+};
+
+//가게ID
+
+let storeIDlist = readDataID('./store.csv');
+function equlstoreID(storeid){
+    return getDataID(storeid);
+};
+
+//사용자ID
+
+let userIDlist = readDataID('./user.csv');
+function equluserID(userid){
+    return getDataID(userid);
+};
+
+//주문 데이터
+
+function orderResult(){
+    const result = `${orderID()}, ${orderDate()}, ${equlstoreID(storeIDlist)}, ${equluserID(userIDlist)}`;
+    return `${result}`;
+};
+
+//랜덤 데이터 생성
+
+let dataRecords = process.argv[2];
+let displayformat = process.argv[3];
+
+if(process.argv.length<4){
+    displayformat ='csv'; // 추후 csv 뿐만 아니라 다른 파일들도 생성 할 수도 있을거 같아 내버려둠
+};
+
+console.log('ID, 주문일자, 가게ID, 사용자ID');
+
+const csvData = ['ID, 주문일자, 가게ID, 사용자ID'];
+
+for (let i=0; i<dataRecords;i++){
+    const orderdata = orderResult();
+    console.log(orderdata);
+    csvData.push(orderdata); //csv에 데이터 심기
+};
+
+const csvString = csvData.join('\n');
+
+fs.writeFile('order.csv', csvString, 'utf-8', (err) => {
+    if (err) {
+    console.error('오류!오류!:', err);
+    } else {
+    console.log('CSV 생성 완료: order.csv');
+    };
+});
